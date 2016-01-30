fc4539218d34028d786132c7ee8d13e26ecd9653
mayago <mayago@missionrnd.com>
2016-01-30 23:48:55 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -20,6 +20,44 @@ struct transaction {
 	char description[20];
 };
 
+int isGreater(char *date1, char *date2)
+{
+	//	printf("date1 : %s date2: %s\n",date1,date2);
+	int y1 = ((date1[6] - '0') * 1000 + (date1[7] - '0') * 100 + (date1[8] - '0') * 10 + (date1[9] - '0'));
+	int y2 = ((date2[6] - '0') * 1000 + (date2[7] - '0') * 100 + (date2[8] - '0') * 10 + (date2[9] - '0'));
+	//	printf("y1: %d y2: %d\n",y1,y2);
+	if (y1>y2)
+		return 1;
+	else if (y1 < y2)
+		return 0;
+	else
+	{
+		int m1 = ((date1[3] - '0') * 10 + (date1[4] - '0'));
+		int m2 = ((date2[3] - '0') * 10 + (date2[4] - '0'));
+		//		printf("m1: %d m2: %d\n",m1,m2);
+		if (m1>m2)
+			return 1;
+		else if (m1 < m2)
+			return 0;
+		else
+		{
+			int d1 = ((date1[0] - '0') * 10 + (date1[1] - '0'));
+			int d2 = ((date2[0] - '0') * 10 + (date2[1] - '0'));
+			//			printf("d1: %d d2: %d\n",d1,d2);
+			if (d1>d2)
+				return 1;
+			else
+				return 0;
+		}
+	}
+}
+
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (isGreater(Arr[i].date, date))
+			break;
+	}
+	return len - i;
 }
